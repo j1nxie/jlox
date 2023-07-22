e720c24d624038fab097429c2de246c5fdd8cbac
j1nxie <rylieeeeexd@gmail.com>
2023-07-23 01:42:34 +0700

    style: add newline between cases

--- app/src/main/java/moe/rylie/jlox/Scanner.java
+++ app/src/main/java/moe/rylie/jlox/Scanner.java
@@ -34,45 +34,59 @@ class Scanner {
             case '(':
                 addToken(LEFT_PAREN);
                 break;
+
             case ')':
                 addToken(RIGHT_PAREN);
                 break;
+
             case '{':
                 addToken(LEFT_BRACE);
                 break;
+
             case '}':
                 addToken(RIGHT_BRACE);
                 break;
+
             case ',':
                 addToken(COMMA);
                 break;
+
             case '.':
                 addToken(DOT);
                 break;
+
             case '-':
                 addToken(MINUS);
                 break;
+
             case '+':
                 addToken(PLUS);
                 break;
+
             case ';':
                 addToken(SEMICOLON);
                 break;
+
             case '*':
                 addToken(STAR);
                 break;
+
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
+
             case '=':
                 addToken(match('=') ? EQUAL_EQUAL : EQUAL);
                 break;
+
             case '<':
                 addToken(match('=') ? LESS_EQUAL : LESS);
                 break;
+
             case '>':
                 addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
+
             case '/':
                 if (match('/')) {
                     while (peek() != '\n' && !isAtEnd()) {
