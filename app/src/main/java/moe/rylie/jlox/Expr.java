5e3b70d5464f87acb9ee80fcefac8df6334081a0
j1nxie <rylieeeeexd@gmail.com>
2023-07-23 16:23:07 +0700

    feat: implement visitors for expressions

--- app/src/main/java/moe/rylie/jlox/Expr.java
+++ app/src/main/java/moe/rylie/jlox/Expr.java
@@ -3,6 +3,18 @@ package moe.rylie.jlox;
 import java.util.List;
 
 abstract class Expr {
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+
+        R visitGroupingExpr(Grouping expr);
+
+        R visitLiteralExpr(Literal expr);
+
+        R visitUnaryExpr(Unary expr);
+    }
+
+    abstract <R> R accept(Visitor<R> visitor);
+
     static class Binary extends Expr {
         final Expr left;
         final Token operator;
@@ -13,6 +25,11 @@ abstract class Expr {
             this.operator = operator;
             this.right = right;
         }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
     }
 
     static class Grouping extends Expr {
@@ -21,6 +38,11 @@ abstract class Expr {
         Grouping(Expr expression) {
             this.expression = expression;
         }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
     }
 
     static class Literal extends Expr {
@@ -28,6 +50,11 @@ abstract class Expr {
 
         Literal(Object value) {
             this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
         }
     }
 
@@ -38,6 +65,11 @@ abstract class Expr {
         Unary(Token operator, Expr right) {
             this.operator = operator;
             this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
         }
     }
 }
--- app/src/main/java/moe/rylie/tool/GenerateAst.java
+++ app/src/main/java/moe/rylie/tool/GenerateAst.java
@@ -30,7 +30,12 @@ public class GenerateAst {
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
-        writer.println("abstract class " + baseName + "{");
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
 
         for (String type : types) {
             String className = type.split(":")[0].trim();
@@ -40,6 +45,17 @@ public class GenerateAst {
 
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(
+                    "        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
 
     private static void defineType(
@@ -58,6 +74,12 @@ public class GenerateAst {
             String name = field.split(" ")[1];
             writer.println("            this." + name + " = " + name + ";");
         }
+        writer.println("        }");
+
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
         writer.println("        }");
         writer.println("    }");
     }
