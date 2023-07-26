04d219bf901a1d22735abb40a806aa8917364091
j1nxie <rylieeeeexd@gmail.com>
2023-07-26 23:56:23 +0700

    style: lowercase for consistency

--- app/src/main/java/moe/rylie/jlox/Lox.java
+++ app/src/main/java/moe/rylie/jlox/Lox.java
@@ -15,7 +15,7 @@ public class Lox {
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
+            System.out.println("usage: jlox [script]");
             System.exit(64);
         } else if (args.length == 1) {
             runFile(args[0]);
