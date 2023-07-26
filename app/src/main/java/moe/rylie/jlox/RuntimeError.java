4744b62ff355844559e2f71129cbcefc8381b621
j1nxie <rylieeeeexd@gmail.com>
2023-07-26 15:24:12 +0700

    feat: evaluate expressions

---
+++ app/src/main/java/moe/rylie/jlox/RuntimeError.java
@@ -0,0 +1,10 @@
+package moe.rylie.jlox;
+
+public class RuntimeError extends RuntimeException {
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
